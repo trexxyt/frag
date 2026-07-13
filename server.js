c08b1554e0e637e217003317f706cff7acfabd35
trexxyt <thevineyard@bellnet.ca>
2026-07-13 05:42:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://dexxyg.netlify.app',
   'https://bellbusinesscanada.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
-  'https://breetq.netlify.app',
+  'https://vret.netlify.app',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
 ];
